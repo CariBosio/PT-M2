85981f63ed038036850526971a6a14ac93997ac6
CariBosio <caribosio72gmail.com>
2022-04-29 16:19:33 -0300

    Hecho

--- 05-Bundlers/homework/browser/app.js
+++ 05-Bundlers/homework/browser/app.js
@@ -1,29 +1,43 @@
-(function () {
+//(function () {
 
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
+//!ES6-----------------------------------------------------------------
+// import { whiteboard } from './whiteboard.js';
+//import io from 'socket.io-client';
+//var socket = io(window.location.origin);
+//!-----------------------------------------------------------------
 
-  socket.on('connect', function () {
+
+
+// var whiteboard = window.whiteboard;  //variable global whiteboard, que viene de whiteboard.js
+var whiteboard = require('./whiteboard.js');
+
+//var socket = window.io(window.location.origin); //requiere el socket.io-client
+var io = require('socket.io-client');
+var socket = io(window.location.origin); //requiero el socket.io-client - configura la primer parte del whiteboard para que después podamos pintar, etc
+
+
+
+socket.on('connect', function() {
     console.log('Connected!');
-  });
+});
 
-  socket.on('load', function (strokes) {
+socket.on('load', function(strokes) {
 
-    strokes.forEach(function (stroke) {
-      var start = stroke.start;
-      var end = stroke.end;
-      var color = stroke.color;
-      whiteboard.draw(start, end, color, false);
+    strokes.forEach(function(stroke) {
+        var start = stroke.start;
+        var end = stroke.end;
+        var color = stroke.color;
+        whiteboard.draw(start, end, color, false);
     });
 
-  });
+});
 
-  socket.on('draw', function (start, end, color) {
+socket.on('draw', function(start, end, color) {
     whiteboard.draw(start, end, color, false);
-  });
+});
 
-  whiteboard.on('draw', function (start, end, color) {
+whiteboard.on('draw', function(start, end, color) {
     socket.emit('draw', start, end, color);
-  });
+});
 
-})();
+//})();
